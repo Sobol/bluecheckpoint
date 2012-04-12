8b48ad2568433f68b3c6437875547fb92bbedeaf
Paweł Sobolewski <pawel.sobolewski@amberbit.com>
2012-04-12 16:10:46 +0200

    Devices working

--- config/routes.rb
+++ config/routes.rb
@@ -12,18 +12,17 @@ Bluecheckpoint::Application.routes.draw do
   root :to => "welcome#index"
   
   match 'company' => 'static_pages#company', :as => :company_page
+  match 'contact' => 'static_pages#contact', :as => :contact_page
+  match 'devices' => 'static_pages#devices', :as => :devices_page
+  match 'localization' => 'static_pages#localization', :as => :localization_page
+  match 'cooperation' => 'static_pages#cooperation', :as => :cooperation_page
 
   resources :news, :only => :index
   resources :static_pages do
     collection do
-      get 'company'
-      get 'devices'
       get 'partners'
       get 'help'
       get 'contact'
-      get 'news'
-      get 'localization'
-      get 'cooperation'
       get 'technodiag'
       get 'imedia'
       get 'codes'
